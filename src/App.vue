d1aa61a25dd07118c6a2ac70bb3f03564ef2179f
Hosana <hosanabarceloscandido@gmail.com>
2023-12-16 17:05:35 -0300

    feat: add implementação inicial de home page

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,66 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <Banner />
 </template>
 
+<script lang="ts">
+import Banner from './components/Banner.vue';
+export default {
+    components: {
+        Banner
+    }
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+  height: 4.5rem;
+  margin-bottom: 3rem;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+.frase-cabecalho {
+  margin-bottom: 2rem;
+}
+
+.texto-verde {
+  color: var(--verde-medio, #3D6D4A);
+}
+
+.foto-banner {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
   }
 
   .logo {
-    margin: 0 2rem 0 0;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .foto-banner {
+    width: min(100%, 21.25rem);
   }
 }
 </style>
