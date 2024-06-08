0d76515035fb833a5e529ff9e4e45d48a346574a
Hosana <hosanabarceloscandido@gmail.com>
2024-06-08 15:27:24 -0300

    feat: cria componente de lista e importa no conteúdo principal

---
+++ src/components/List.vue
@@ -0,0 +1,65 @@
+<template>
+    <section>
+      <span class="subtitulo-lg sua-lista-texto">
+        Sua lista:
+      </span>
+      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+        <li
+          :key="ingrediente"
+          v-for="ingrediente in ingredientes"
+        >
+          <tag
+            :text="ingrediente"
+            active
+          />
+        </li>
+      </ul>
+      <p v-else class="paragrafo lista-vazia">
+        <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
+        Sua lista está vazia, selecione ingredientes para iniciar.
+      </p>
+    </section>
+  </template>
+
+  <script lang="ts">
+  import Tag from '../components/Tag.vue';
+
+  export default {
+    components: {
+      Tag
+    },
+    props: {
+      ingredientes: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+  </script>
+
+  <style scoped>
+  .sua-lista-texto {
+    color: var(--coral, #F0633C);
+    display: block;
+    text-align: center;
+    margin-bottom: 1.5rem;
+  }
+
+  .ingredientes-sua-lista {
+    display: flex;
+    justify-content: center;
+    gap: 1rem 1.5rem;
+    flex-wrap: wrap;
+  }
+
+  .lista-vazia {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 0.25rem;
+
+    color: var(--coral, #F0633C);
+    text-align: center;
+  }
+</style>
