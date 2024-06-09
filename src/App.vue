eeeb1932a7cb7bce5ac6c2456bb939176d419047
Hosana <hosanabarceloscandido@gmail.com>
2024-06-09 16:03:37 -0300

    rafactor: ajusta chamada de componentes para o app

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,15 @@
-<template>
-    <banner />
-    <main-content />
-    <default-footer />
-</template>
-
 <script lang="ts">
 import Banner from './components/Banner.vue';
 import MainContent from './components/MainContent.vue';
 import DefaultFooter from './components/DefaultFooter.vue';
 
 export default {
-    components: {
-        Banner,
-        MainContent,
-        DefaultFooter,
-    }
+  components: { Banner, MainContent, DefaultFooter }
 }
 </script>
 
-<style scoped>
-.banner {
-  padding: 4rem 7.5rem;
-  color: var(--creme);
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  column-gap: 3.25rem;
-}
-
-.logo {
-  height: 4.5rem;
-  margin-bottom: 3rem;
-}
-
-.frase-cabecalho {
-  margin-bottom: 2rem;
-}
-
-.texto-verde {
-  color: var(--verde-medio, #3D6D4A);
-}
-
-.foto-banner {
-  width: 35rem;
-}
-
-@media only screen and (max-width: 1300px) {
-  .banner {
-    padding: 4rem 3.75rem;
-    flex-direction: column;
-    align-items: center;
-    gap: 1rem;
-  }
-
-  .logo {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-  }
-}
-
-@media only screen and (max-width: 767px) {
-  .banner {
-    padding: 4rem 1.5rem;
-  }
-
-  .foto-banner {
-    width: min(100%, 21.25rem);
-  }
-}
-</style>
+<template>
+  <Banner />
+  <MainContent />
+  <DefaultFooter />
+</template>
