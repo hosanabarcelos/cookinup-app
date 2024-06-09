7f6563c0c22cd74686f06b2ff06a684fcac697e7
Hosana <hosanabarceloscandido@gmail.com>
2024-06-09 15:46:36 -0300

    feat: add requisição get para categorias e receitas

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,9 +1,15 @@
-import type Category from '@/interfaces/Category';
+import type Category from "@/interfaces/Category";
+import type Recipe from "@/interfaces/Recipe";
+
+async function URLData<T>(url: string) {
+  const response = await fetch(url);
+  return response.json() as T;
+}
 
 export async function getCategories() {
-    const response = await fetch('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/bf463b47860043da3b3604ca60cffc3ad1ba9865/categorias.json');
+  return URLData<Category[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/86802bed06855cdccc9247dd1fa175c6ccea7dda/categorias.json');
+}
 
-    const categories: Category[] = await response.json();
-
-    return  categories;
+export async function getRecipes() {
+  return URLData<Recipe[]>('https://gist.githubusercontent.com/antonio-evaldo/002ad55e1cf01ef3fc6ee4feb9152964/raw/07e853b7d0626db51ce2e84bb2f15ca450b7bd7f/receitas.json');
 }
